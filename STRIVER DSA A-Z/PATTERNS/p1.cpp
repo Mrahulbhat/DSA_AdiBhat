afb15d7094c99c6e8d362c9e4855c6095aed5969
Madithyabhat <adibhat2005@gmail.com>
2025-04-20 18:06:49 +0530

    solved pattern->problem1

---
+++ STRIVER DSA A-Z/PATTERNS/p1.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+using namespace std;
+
+void nForest(int n) {
+    for (int i = 0; i < n; i++) { //outer for loop for rows
+        for (int j = 0; j < n; j++) { //inner for loop for columns
+            cout << "* "; //print star and space
+        }
+        for (int j = 0; j < n; j++) {
+            cout << "* ";
+        }
+        cout << "\n";
+    }
+}
